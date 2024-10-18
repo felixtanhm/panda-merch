852aeb31a88f1ee9fc3dd3dbb8385ed7363835a5
felixtanhm <felixtanhm@gmail.com>
2024-10-18 11:37:25 +0800

    RF | Separate Handler func into helper function

--- internal/handlers/merch.go
+++ internal/handlers/merch.go
@@ -1,7 +1,9 @@
 package handlers
 
 import (
+	"database/sql"
 	"encoding/json"
+	"log"
 	"net/http"
 	"time"
 )
@@ -25,25 +27,10 @@ func (app *App) MerchHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	switch r.Method {
 	case http.MethodGet:
-		merchList := []Merch{}
-
-		rows, err := app.DB.Query("SELECT * FROM Merchandise")
+		merchList, err := app.fetchMerch()
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		}
-		defer rows.Close()
-
-		for rows.Next() {
-			merch := Merch{}
-			err = rows.Scan(&merch.ID, &merch.Name, &merch.Price, &merch.Availability, &merch.CreatedAt, &merch.ModifiedAt)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			merchList = append(merchList, merch)
-		}
-
 		response := Response{
 			CurPage:    1,
 			TotalItems: len(merchList),
@@ -52,9 +39,40 @@ func (app *App) MerchHandler(w http.ResponseWriter, r *http.Request) {
 
 		err = json.NewEncoder(w).Encode(response)
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+			log.Printf("JSON encoding error: %v", err)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 			return
 		}
 	}
+}
 
+func (app *App) fetchMerch() ([]Merch, error) {
+	merchList := []Merch{}
+	pageSize := 10
+	page := 1
+	offset := (page - 1) * pageSize
+	query := "SELECT id, name, price, availability, createdAt, modifiedAt FROM Merchandise ORDER BY id OFFSET @offset ROWS FETCH NEXT @pageSize ROWS ONLY;"
+	rows, err := app.DB.Query(query, sql.Named("offset", offset), sql.Named("pageSize", pageSize))
+	if err != nil {
+		log.Printf("Database query error: %v", err)
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		merch := Merch{}
+		err = rows.Scan(&merch.ID, &merch.Name, &merch.Price, &merch.Availability, &merch.CreatedAt, &merch.ModifiedAt)
+		if err != nil {
+			log.Printf("Row scanning error: %v", err)
+			return nil, err
+		}
+		merchList = append(merchList, merch)
+	}
+	err = rows.Err()
+	if err != nil {
+		log.Printf("Row iteration error: %v", err)
+		return nil, err
+	}
+
+	return merchList, nil
 }
